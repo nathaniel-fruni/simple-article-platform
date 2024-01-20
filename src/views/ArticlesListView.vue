156ce5b667058a716659466903ab2a497d8cff72
Nathaniel Fruni <fruninathan@gmail.com>
2024-01-20 23:30:06 +0100

    20.1.
    pagination ArticleListView

--- src/views/ArticlesListView.vue
+++ src/views/ArticlesListView.vue
@@ -1,69 +1,98 @@
 <template>
-    <PageHeaderInfo />
-    <section class="section-padding">
-        <div class="container">
-            <div class="mb-5">
-                <SelectedTopicBar />
-            </div>
-
-            <div class="row">
-
-                <div class="col-lg-12 col-12 text-center">
-                    <h2 class="mb-4">Articles To Read</h2>
+    <div>
+        <PageHeaderInfo />
+        <section class="section-padding">
+            <div class="container">
+                <div class="mb-5">
+                    <SelectedTopicBar />
                 </div>
 
-                <div class="col-lg-8 col-12 mt-3 mx-auto">
-                    <div v-for="topic in filteredTopics" :key="topic.id">
-                        <div v-for="article in topic.articles" :key="article.id">
+                <div class="row">
+                    <div class="col-lg-12 col-12 text-center">
+                        <h2 class="mb-4">Articles To Read</h2>
+                    </div>
+
+                    <div class="col-lg-8 col-12 mt-3 mx-auto">
+                        <div v-for="article in paginatedArticles" :key="article.id">
                             <ArticleCard :article="article" />
                         </div>
                     </div>
+
+                    <div class="col-lg-12 col-12">
+                        <nav>
+                            <ul class="pagination justify-content-center mb-0">
+                                <li class="page-item" :class="{ disabled: currentPage === 1 }" style="cursor:pointer">
+                                    <a class="page-link" @click="goToPage(currentPage - 1)">
+                                        <span>Prev</span>
+                                    </a>
+                                </li>
+
+                                <li v-for="page in totalPages" :key="page" class="page-item" :class="{ active: page === currentPage }" style="cursor:pointer">
+                                    <a class="page-link" @click="goToPage(page)">{{ page }}</a>
+                                </li>
+
+                                <li class="page-item" :class="{ disabled: currentPage === totalPages }" style="cursor:pointer">
+                                    <a class="page-link" @click="goToPage(currentPage + 1)">
+                                        <span>Next</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-
-                <div class="col-lg-12 col-12">
-                    <nav>
-                        <ul class="pagination justify-content-center mb-0">
-                            <li class="page-item">
-                                <a class="page-link" href="#">
-                                    <span>Prev</span>
-                                </a>
-                            </li>
-
-                            <li class="page-item">
-                                <a class="page-link" href="#">
-                                    <span>Next</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-
             </div>
-        </div>
-    </section>
-    <ContactInfo />
+        </section>
+        <ContactInfo />
+    </div>
 </template>
+
 <script>
-import {useSelectedTopicStore} from "@/stores/SelectedTopicsStore";
+import { useSelectedTopicStore } from "@/stores/SelectedTopicsStore";
 import SelectedTopicBar from "@/components/SelectTopicBar.vue";
 import PageHeaderInfo from "@/components/PageHeaderInfo.vue";
 import ArticleCard from "@/components/ArticleCard.vue";
 import ContactInfo from "@/components/ContactInfo.vue";
 
 export default {
-    data () {
+    data() {
         const topicStore = useSelectedTopicStore();
-
         return {
-            topicStore
-        }
+            topicStore,
+            currentPage: 1,
+            itemsPerPage: 4
+        };
     },
-    components: {PageHeaderInfo, SelectedTopicBar, ContactInfo, ArticleCard},
+    components: { PageHeaderInfo, SelectedTopicBar, ContactInfo, ArticleCard },
     computed: {
-        filteredTopics() {
+        filteredArticles() {
             this.topicStore.restoreState();
-            return this.topicStore.filteredTopics;
-        }
-    }
-}
+            return this.topicStore.filteredTopics.flatMap(topic => topic.articles);
+        },
+        totalPages() {
+            return Math.ceil(this.filteredArticles.length / this.itemsPerPage);
+        },
+        paginatedArticles() {
+            const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+            const endIndex = startIndex + this.itemsPerPage;
+            return this.filteredArticles.slice(startIndex, endIndex);
+        },
+    },
+    watch: {
+        filteredArticles: 'adjustCurrentPage',
+    },
+    methods: {
+        goToPage(page) {
+            if (page >= 1 && page <= this.totalPages) {
+                this.currentPage = page;
+            }
+        },
+        adjustCurrentPage() {
+            const totalPages = Math.ceil(this.filteredArticles.length / this.itemsPerPage);
+
+            if (this.currentPage > totalPages) {
+                this.currentPage = totalPages || 1;
+            }
+        },
+    },
+};
 </script>
